a0b9eee02d1d295486e9b9657e944fb0116b70cc
zcc <listenzcc@mail.bnu.edu.cn>
2020-09-24 16:15:55 +0800

    Daily update

--- src/onstart.js
+++ src/onstart.js
@@ -97,6 +97,7 @@ function create_svg() {
   var x_subject = 100;
   var x_object = 300;
   var background_color = "beige";
+  var height_unit = 50;
 
   // Add new svg in #svg_container
   let svg = d3
@@ -107,7 +108,6 @@ function create_svg() {
   // Add #svg_g1 in svg,
   // it contains subjects
   var y = 50;
-  var dy = 50;
   svg
     .append("g")
     .attr("id", "svg_g1")
@@ -120,7 +120,7 @@ function create_svg() {
     .attr("x", x_subject + "px")
     .attr("y", () => {
       y_str = y + "px";
-      y += dy;
+      y += height_unit;
       return y_str;
     });
 
@@ -143,103 +143,15 @@ function create_svg() {
     .attr("x", x_object + "px")
     .attr("y", () => {
       y_str = y + "px";
-      y += dy;
+      y += height_unit;
       return y_str;
     });
 
   // Set height of the svg
   svg.attr("height", y + "px");
 }
-// var subject_offset_x = 100;
-// var object_offset_x = 300;
-// var object_y = 20;
-// var object_dy = 20;
 
-// Place texts
-// for (var idx in subject_index) {
-//   var color = random_color(1, 10);
-
-//   var y0 = object_y;
-//   for (var i = 0; i < object_list[idx].length; i++) {
-//     object_y += object_dy;
-//     object_list[idx][i].dom = svg
-//       .append("text")
-//       .text(object_list[idx][i].name)
-//       .attr("x", object_offset_x)
-//       .attr("y", object_y)
-//       .attr("fill", color)
-//       .style("text-anchor", "middle");
-//   }
-
-//   object_y += object_dy;
-
-//   subject_list[idx].color = color;
-//   subject_list[idx].dom = svg
-//     .append("text")
-//     .text(subject_list[idx].subject)
-//     .attr("x", subject_offset_x)
-//     .attr("y", (y0 + object_y) / 2)
-//     .attr("fill", color)
-//     .style("text-anchor", "middle");
-// }
-
-// // Draw connections
-// for (var j in subject_index) {
-//   for (var k in object_list[j]) {
-//     x0 = subject_list[j].dom._groups[0][0].x.baseVal[0].value;
-//     y0 = subject_list[j].dom._groups[0][0].y.baseVal[0].value;
-//     color = subject_list[j].color;
-//     x1 = object_list[j][k].dom._groups[0][0].x.baseVal[0].value;
-//     y1 = object_list[j][k].dom._groups[0][0].y.baseVal[0].value;
-//     svg
-//       .append("path")
-//       .attr(
-//         "d",
-//         "M " +
-//           x0 +
-//           " " +
-//           y0 +
-//           " C" +
-//           200 +
-//           " " +
-//           y0 +
-//           " " +
-//           200 +
-//           " " +
-//           y1 +
-//           " " +
-//           x1 +
-//           " " +
-//           y1
-//       )
-//       .attr("stroke", color)
-//       .attr("fill", "transparent");
-//   }
-// }
-
-// svg.attr(
-//   "style",
-//   "width: " +
-//     width +
-//     "px;" +
-//     "height: " +
-//     object_y +
-//     "px;" +
-//     "background-color: " +
-//     background_color
-// );
-
-// console.log("subject_list", subject_list);
-
-// svg
-//   .append("circle")
-//   .attr("cx", "10")
-//   .attr("cy", "20")
-//   .attr("r", "40")
-//   .attr("stroke", "black")
-//   .attr("stroke-width", "2")
-//   .attr("fill", "none");
-
+// Create tables for each subject
 function create_tables() {
   // Create a table for each subject
   // in #contents div
@@ -290,15 +202,22 @@ function create_tables() {
     .text((d) => d);
 }
 
+// The scripts starts here
+// Expose global variables
 var subject_index = [];
 var subject_list = [];
 var object_list = [];
 
-// Fill #contents div
+// Load json data
 d3.json("latest.json").then(function (data) {
   console.log("data", data);
+  // Fill the data into global lists
+  // subject_index, subject_list and object_list will be filled
   expose_parsed_data(data);
 
+  // Create tables based on data
   create_tables();
+
+  // Create svg based on data
   create_svg();
 });
